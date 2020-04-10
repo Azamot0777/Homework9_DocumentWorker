62140a3d6e402209184fc8e1c059d960916e7d1d
Heisenberg <Persey_98@list.ru>
2020-04-10 16:27:56 +0500

    beggining of work

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework9_DocumentWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
