b7cf740891a315da27bab6da1011f99a2acf90cb
Heisenberg <Persey_98@list.ru>
2020-04-10 16:42:54 +0500

    I've created the classes and methods

--- Program.cs
+++ Program.cs
@@ -2,11 +2,36 @@
 
 namespace Homework9_DocumentWorker
 {
+
+    class DocumentWorker
+    {
+        static void OpenDocument()
+        {
+            System.Console.WriteLine("Document is open");
+        }
+        static void EditDocument()
+        {
+            System.Console.WriteLine("Document editing is available in pro version");
+        }
+        static void SaveDocument()
+        {
+            System.Console.WriteLine("Saving the document is available in pro version");
+        }
+
+    }
+    class ProDocumentWorker : DocumentWorker
+    {
+
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
